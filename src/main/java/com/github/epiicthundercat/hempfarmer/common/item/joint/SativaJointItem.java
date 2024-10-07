3fecdd658458b40f95e176ac78a360fe9a5e5934
Ricky <rvillalobos102299@gmail.com>
2024-10-07 23:20:32 +0200

    First Build of 1.18.2 Ready!

--- src/main/java/com/github/epiicthundercat/hempfarmer/common/item/joint/SativaJointItem.java
+++ src/main/java/com/github/epiicthundercat/hempfarmer/common/item/joint/SativaJointItem.java
@@ -57,7 +57,7 @@ public class SativaJointItem extends Item {
 
         }
 
-        return InteractionResultHolder.consume(itemstack);  
+        return InteractionResultHolder.consume(itemstack);
     }
 
     @Override
